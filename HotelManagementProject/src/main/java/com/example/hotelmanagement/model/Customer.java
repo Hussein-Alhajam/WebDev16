dc698c56865e86fee24cb0b02cefa3a6f8d9c31c
Anthony Liscio <anthony.liscio@ontariotechu.net>
2024-03-27 17:57:43 -0400

    Customer model updated

--- HotelManagementProject/src/main/java/com/example/hotelmanagement/model/Customer.java
+++ HotelManagementProject/src/main/java/com/example/hotelmanagement/model/Customer.java
@@ -1,5 +1,91 @@
 package com.example.hotelmanagement.model;
 
-public class Customer {
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private String phoneNumber;
+	private String email;
+	
+	// Customer has a OneToMany relationship with bookings.
+	// (Each customer can have multiple bookings)
+	@OneToMany(mappedBy = "customer")
+	private List<Booking> bookings;
+	
+	// constructor
+	public Customer() {}
+	
+	// parameterized constructor
+	public Customer(Long id, String name, String phoneNumber, String email) {
+		this.id = id;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+	
+
+	//
+	// Getters and setters
+	//
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public List<Booking> getBookings() {
+		return bookings;
+	}
+
+	public void setBookings(List<Booking> bookings) {
+		this.bookings = bookings;
+	}
+
+	
+	
+	//
+	// toString
+	//
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", phoneNumber=" + phoneNumber + ", email=" + email
+				+ ", bookings=" + bookings + "]";
+	}
 }
