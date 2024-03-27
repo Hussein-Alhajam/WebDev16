ea5a753122ad8f50459171b47231f89435d981b4
Hussein-Alhajam <90421975+Hussein-Alhajam@users.noreply.github.com>
2024-03-27 16:06:04 -0400

    Add files via upload

---
+++ HotelManagementProject/src/main/java/com/example/hotelmanagement/HotelManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementProjectApplication.class, args);
+	}
+
+}
