4ad9dcd6ab371115133d103e6ddb6b1066110def
Eromosele Kelvin Smart-Ochie <eromosele.smartochie@ontariotechu.net>
2024-04-09 22:17:57 -0400

    update on service

---
+++ HotelManagementProject/src/main/java/com/example/Service/BookingService.java
@@ -0,0 +1,26 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class BookingService {
+    
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    public List<Booking> getAllBookings() {
+        return bookingRepository.findAll();
+    }
+
+    public Booking getBookingById(Long id) {
+        return bookingRepository.findById(id).orElse(null);
+    }
+
+    public Booking createOrUpdateBooking(Booking booking) {
+        return bookingRepository.save(booking);
+    }
+
+    public void deleteBooking(Long id) {
+        bookingRepository.deleteById(id);
+    }
+}
