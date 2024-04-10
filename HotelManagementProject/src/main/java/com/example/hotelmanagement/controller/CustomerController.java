ba577a6ebfdd07d9992116a88f6112a132ccbf87
Anthony Liscio <anthony.liscio@ontariotechu.net>
2024-04-09 23:31:20 -0400

    controllers added in (code done by kelvin)

---
+++ HotelManagementProject/src/main/java/com/example/hotelmanagement/controller/CustomerController.java
@@ -0,0 +1,23 @@
+package com.example.hotelmanagement.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.example.hotelmanagement.model.Customer;
+import com.example.hotelmanagement.service.CustomerService;
+
+@Controller
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/customers")
+    public String getAllCustomers(Model model) {
+        model.addAttribute("customers", customerService.getAllCustomers());
+        return "customerList"; // Assuming Thymeleaf template name is customerList.html
+    }
+
+    // Other CRUD operations for Customer entity can be added here
+}
